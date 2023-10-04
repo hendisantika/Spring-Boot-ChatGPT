0089fc64eb43762a39f8c8f6fb3435d27eff8a0c
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-04 07:33:25 +0700

    #1 - Initial commit ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springbootchatgpt/SpringBootChatGptApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootchatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootChatGptApplication.class, args);
+    }
+
+}
