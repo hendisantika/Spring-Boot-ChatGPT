90d4f5e7dd73222f09d0b611c0ad36a4e437fc95
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-04 07:37:57 +0700

    #9 - Add processSearch in ChatGPTService ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springbootchatgpt/service/ChatGPTService.java
+++ src/main/java/com/hendisantika/springbootchatgpt/service/ChatGPTService.java
@@ -1,6 +1,15 @@
 package com.hendisantika.springbootchatgpt.service;
 
+import com.google.gson.Gson;
+import com.hendisantika.springbootchatgpt.model.ChatGPTRequest;
+import com.hendisantika.springbootchatgpt.model.ChatGPTResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -24,4 +33,44 @@ public class ChatGPTService {
 
     @Value("${OPEN_AI_KEY}")
     private String OPEN_AI_KEY;
+
+    public String processSearch(String query) {
+
+        ChatGPTRequest chatGPTRequest = new ChatGPTRequest();
+        chatGPTRequest.setPrompt(query);
+
+
+        String url = OPEN_AI_URL;
+
+        HttpPost post = new HttpPost(url);
+        post.addHeader("Content-Type", "application/json");
+        post.addHeader("Authorization", "Bearer " + OPEN_AI_KEY);
+
+        Gson gson = new Gson();
+
+        String body = gson.toJson(chatGPTRequest);
+
+        log.info("body: " + body);
+
+        try {
+            final StringEntity entity = new StringEntity(body);
+            post.setEntity(entity);
+
+            try (CloseableHttpClient httpClient = HttpClients.custom().build();
+                 CloseableHttpResponse response = httpClient.execute(post)) {
+
+                String responseBody = EntityUtils.toString(response.getEntity());
+
+                log.info("responseBody: " + responseBody);
+
+                ChatGPTResponse chatGPTResponse = gson.fromJson(responseBody, ChatGPTResponse.class);
+
+                return chatGPTResponse.getChoices().get(0).getText();
+            } catch (Exception e) {
+                return "failed";
+            }
+        } catch (Exception e) {
+            return "failed";
+        }
+    }
 }
